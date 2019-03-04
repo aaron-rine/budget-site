b2a320024baa2ad4a05908b541bbb2494589d63a
Aaron Rine <aaronrine@gmail.com>
2019-03-04 15:09:06 -0800

    budget site, first model

--- budgetSite/myBudgetSite/models.py
+++ budgetSite/myBudgetSite/models.py
@@ -1,3 +1,18 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Bill(models.Model):
+    service = models.CharField(max_length=200)
+    amount = models.DecimalField(max_digits=7, decimal_places=2)
+    description = models.TextField()
+    dueDate = models.DateTimeField(default=timezone.now)
+    payedDate = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.service
